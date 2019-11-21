a829de8e1d6438ce17d4d45a345f5a50643a2ed9
Cezary Siwon <cs@recordit.pl>
2019-11-21 21:00:03 +0100

    modify title

--- js/main.js
+++ js/main.js
@@ -12,6 +12,16 @@ function AJAX(url){
         }
         ajax.send();
     };
+    this.delete = function(id, callback){
+        let ajax = new XMLHttpRequest();
+        ajax.open("DELETE", this.url+`/${id}`);
+        ajax.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200){
+                callback(this.response);
+            }
+        }
+        ajax.send();
+    }
     this.findById = function (id, callback) {
         let ajax = new XMLHttpRequest();
         ajax.open("GET", this.url + `/${id}`);
@@ -37,6 +47,17 @@ function AJAX(url){
     this.patch = function(id, object, callback){
         let ajax = new XMLHttpRequest();
         ajax.open("PATCH",this.url + `/${id}`);
+        ajax.setRequestHeader("Content-Type","application/json");
+        ajax.onreadystatechange = function(){
+            if (this.readyState === 4 && this.status === 200){
+                callback(this.response);
+            }
+        }
+        ajax.send(JSON.stringify(object));
+    }
+    this.put = function(id, object, callback){
+        let ajax = new XMLHttpRequest();
+        ajax.open("PUT",this.url + `/${id}`);
         ajax.setRequestHeader("Content-Type","application/json");
         ajax.onreadystatechange = function(){
             if (this.readyState === 4 && this.status === 200){
@@ -78,8 +99,11 @@ function injectHtmlCodeInTableTodos() {
             return `<tr>
                     <td>${item.id}</td>
                     <td>${item.userId}</td>
-                    <td>${item.title}</td>
+                    <td id="title-${item.id}"><button onclick="modifyTitle(${item.id})">Edytuj</button> ${item.title}</td>
                     <td><input type="checkbox" ${item.completed ? "checked" : ""} onchange="setCompleted(${item.id})"/></td>
+                    <td>
+                        <button onclick="deleteTask(${item.id})">Usuń</button>
+                    </td>
                     </tr>`;
         });
         document.getElementById("todos").innerHTML = html.join("");
@@ -105,6 +129,39 @@ function setCompletedTask(response){
        console.log("Patched");
        getTodosFromRestApi();
     });
+}
+
+function deleteTask(id){
+    restTodos.delete(id, function () {
+        console.log("Task deleted");
+        getTodosFromRestApi();
+    })
+}
+
+function modifyTitle(id){
+    let cellTitle = document.getElementById(`title-${id}`);
+    let title = cellTitle.childNodes[1].textContent;
+    cellTitle.innerHTML = `<input id="${id}" type="text" value="${title}"/> 
+                           <button onclick="modifyTitleTask(${id})">Zapisz</button>`;
+}
+
+function setTitle(task){
+    let cellTitle = document.getElementById(`title-${task.id}`);
+    cellTitle.innerHTML = `<td id="title-${task.id}">
+                        <button onclick="modifyTitle(${task.id})">Edytuj</button>
+                        ${task.title}</td>`;
+}
+
+function modifyTitleTask(id) {
+    restTodos.findById(id, function(response){
+        let task = JSON.parse(response);
+        const titleTask = document.getElementById(id).value;
+        task.title = titleTask;
+        restTodos.put(id, task, function(response){
+           setTitle(JSON.parse(response));
+        })
+    });
+
 }
 
 
